a36dd6a3d0a5e0080a1044be87f019c1e81fbca3
pgakafonso <pgak.afonso@gmail.com>
2021-04-15 22:35:19 +0100

    adding initial py files to repository

---
+++ your-code/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 15 22:24:55 2021
+
+@author: pedroafflalo
+"""
+
+from sample_code_cell2 import start_game
+
+
+start_game()
---
+++ your-code/objects_and_rooms.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 15 22:19:25 2021
+
+@author: pedroafflalo
+"""
+
+# define rooms and items
+
+couch = {
+    "name": "couch",
+    "type": "furniture",
+}
+
+door_a = {
+    "name": "door a",
+    "type": "door",
+}
+
+key_a = {
+    "name": "key for door a",
+    "type": "key",
+    "target": door_a,
+}
+
+piano = {
+    "name": "piano",
+    "type": "furniture",
+}
+
+game_room = {
+    "name": "game room",
+    "type": "room",
+}
+
+outside = {
+  "name": "outside"
+}
+
+all_rooms = [game_room, outside]
+
+all_doors = [door_a]
+
+# define which items/rooms are related
+
+object_relations = {
+    "game room": [couch, piano, door_a],
+    "piano": [key_a],
+    "outside": [door_a],
+    "door a": [game_room, outside]
+}
+
+# define game state. Do not directly change this dict. 
+# Instead, when a new game starts, make a copy of this
+# dict and use the copy to store gameplay state. This 
+# way you can replay the game multiple times.
+
+INIT_GAME_STATE = {
+    "current_room": game_room,
+    "keys_collected": [],
+    "target_room": outside
+}
